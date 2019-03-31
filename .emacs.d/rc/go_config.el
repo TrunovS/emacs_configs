3c451f0f25b11b14f8eebd2d638dd7cb88257b20
Trunov Sergey <trunovsergey@list.ru>
2019-03-31 23:14:28 +0300

    Добавил пакеты для установки по-умолчанию.

--- .emacs.d/rc/go_config.el
+++ .emacs.d/rc/go_config.el
@@ -1,6 +1,10 @@
 (require 'go-mode)
 
 (defun tserg/go-mode-hook()
+  (require 'lsp)
+  (require 'lsp-clients)
+  (require 'lsp-ui)
+
   (ws-butler-mode)
   (whitespace-mode)
   ;; stop whitespace being highlighted
@@ -11,6 +15,7 @@
   (setq-local auto-hscroll-mode 'current-line);;emacs version >= 26
   (font-lock-mode t)
   (setq font-lock-maximum-decoration t)
+  (setq godoc-at-point-function (quote godoc-gogetdoc))
 
   (local-set-key [(control return)] 'ac-complete)
   (define-key go-mode-map "\C-j" 'godef-jump)
@@ -24,7 +29,16 @@
   (if (not (string-match "go" compile-command))
       (set (make-local-variable 'compile-command)
            "go build -v && go test -v && go vet"))
+
+  ;; (lsp)
+  ;; (lsp-ui-mode)
+  ;; (setq lsp-ui-doc-include-signature nil)  ; don't include type signature in the child frame
+  ;; (eldoc-mode nil)  
+  ;; (global-eldoc-mode -1)
+  ;; (setq lsp-ui-doc-position (quote top))
   )
 
+(add-to-list 'auto-mode-alist (cons "\\.mod\\'" 'fundamental-mode))
 (add-to-list 'auto-mode-alist (cons "\\.go\\'" 'go-mode))
+
 (add-hook 'go-mode-hook 'tserg/go-mode-hook)
