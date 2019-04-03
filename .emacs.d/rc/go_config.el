36302aa2bea7c2fa87761cd26098c0f50340fe7f
Trunov Sergey <trunovsergey@list.ru>
2019-04-03 23:03:50 +0300

    Автодополнение в Го.

--- .emacs.d/rc/go_config.el
+++ .emacs.d/rc/go_config.el
@@ -1,9 +1,6 @@
-(require 'go-mode)
-
 (defun tserg/go-mode-hook()
-  (require 'lsp)
-  (require 'lsp-clients)
-  (require 'lsp-ui)
+  (with-eval-after-load 'go-mode
+   (require 'go-autocomplete))
 
   (ws-butler-mode)
   (whitespace-mode)
@@ -17,6 +14,7 @@
   (setq font-lock-maximum-decoration t)
   (setq godoc-at-point-function (quote godoc-gogetdoc))
 
+  (add-hook 'before-save-hook 'gofmt-before-save)
   (local-set-key [(control return)] 'ac-complete)
   (define-key go-mode-map "\C-j" 'godef-jump)
   (define-key go-mode-map "\M-j" 'pop-tag-mark)
@@ -30,12 +28,6 @@
       (set (make-local-variable 'compile-command)
            "go build -v && go test -v && go vet"))
 
-  ;; (lsp)
-  ;; (lsp-ui-mode)
-  ;; (setq lsp-ui-doc-include-signature nil)  ; don't include type signature in the child frame
-  ;; (eldoc-mode nil)  
-  ;; (global-eldoc-mode -1)
-  ;; (setq lsp-ui-doc-position (quote top))
   )
 
 (add-to-list 'auto-mode-alist (cons "\\.mod\\'" 'fundamental-mode))
